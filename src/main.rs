e4d9ca59cedf1fb6e9540d630b2072e011585872
Xargin <cao1988228@163.com>
2019-01-17 22:26:35 +0800

    fix unicorn, basic demo done

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,106 @@
 //use capstone::prelude::*;
 use keystone::*;
+use std::collections::HashMap;
+use unicorn::{Cpu, CpuX86};
 
+fn init_register_map(m: &mut HashMap<&str, unicorn::RegisterX86>) {
+    m.insert("rax", unicorn::RegisterX86::RAX);
+    m.insert("rbx", unicorn::RegisterX86::RBX);
+    m.insert("rcx", unicorn::RegisterX86::RCX);
+    m.insert("rdx", unicorn::RegisterX86::RDX);
+    m.insert("rsi", unicorn::RegisterX86::RSI);
+    m.insert("rdi", unicorn::RegisterX86::RDI);
+    m.insert("r8", unicorn::RegisterX86::R8);
+    m.insert("r9", unicorn::RegisterX86::R9);
+    m.insert("r10", unicorn::RegisterX86::R10);
+    m.insert("r11", unicorn::RegisterX86::R11);
+    m.insert("r12", unicorn::RegisterX86::R12);
+    m.insert("r13", unicorn::RegisterX86::R13);
+    m.insert("r14", unicorn::RegisterX86::R14);
+    m.insert("r15", unicorn::RegisterX86::R15);
 
-/*
-const X86_CODE: &'static [u8] = b"\x55\x48\x8b\x05\xb8\x13\x00\x00\xe9\x14\x9e\x08\x00\x45\x31\xe4";
+    m.insert("rip", unicorn::RegisterX86::RIP);
+    m.insert("rbp", unicorn::RegisterX86::RBP);
+    m.insert("rsp", unicorn::RegisterX86::RSP);
+    m.insert("flags", unicorn::RegisterX86::EFLAGS);
 
-/// Print register names
-fn reg_names<T, I>(cs: &Capstone, regs: T) -> String
-    where
-        T: Iterator<Item = I>,
-        I: Into<RegId>,
-{
-    let names: Vec<String> = regs.map(|x| cs.reg_name(x.into()).unwrap()).collect();
-    names.join(", ")
+    m.insert("cs", unicorn::RegisterX86::CS);
+    m.insert("ss", unicorn::RegisterX86::SS);
+    m.insert("ds", unicorn::RegisterX86::DS);
+    m.insert("es", unicorn::RegisterX86::ES);
+    m.insert("fs", unicorn::RegisterX86::FS);
+    m.insert("gs", unicorn::RegisterX86::GS);
 }
 
-/// Print instruction group names
-fn group_names<T, I>(cs: &Capstone, regs: T) -> String
-    where
-        T: Iterator<Item = I>,
-        I: Into<InsnGroupId>,
-{
-    let names: Vec<String> = regs.map(|x| cs.group_name(x.into()).unwrap()).collect();
-    names.join(", ")
-}
+fn print_register(emu: &unicorn::CpuX86) {
+    let mut map = HashMap::new();
+    init_register_map(&mut map);
 
-fn example() -> CsResult<()> {
-    let mut cs = Capstone::new()
-        .x86()
-        .mode(arch::x86::ArchMode::Mode64)
-        .syntax(arch::x86::ArchSyntax::Att)
-        .detail(true)
-        .build()?;
+    let mut sorted_x64_reg_name = vec![
+        "rax", "rbx", "rcx", "rdx", "end",
+        "rsi", "rdi", "r8", "r9", "end",
+        "r10", "r11", "r12", "r13", "end",
+        "r14", "r15", "end",
+        "rip", "rbp", "rsp", "end",
+        "cs", "ss", "ds", "es", "end",
+        "fs", "gs", "end", "flags", "end",
+    ];
 
-    let insns = cs.disasm_all(X86_CODE, 0x1000)?;
-    println!("Found {} instructions", insns.len());
-    for i in insns.iter() {
-        println!("");
-        println!("{}", i);
+    println!("----------------- cpu context -----------------");
 
-        let detail: InsnDetail = cs.insn_detail(&i)?;
-        let output: &[(&str, String)] = &[
-            ("read regs:", reg_names(&cs, detail.regs_read())),
-            ("write regs:", reg_names(&cs, detail.regs_write())),
-            ("insn groups:", group_names(&cs, detail.groups())),
-        ];
-
-        for &(ref name, ref message) in output.iter() {
-            println!("    {:12} {}", name, message);
+    for reg_name in sorted_x64_reg_name {
+        if reg_name == "end" {
+            println!();
+            continue;
         }
+
+        let &uc_reg = map.get(reg_name).unwrap();
+
+        // pad reg_name to 3 bytes
+        let mut reg_name = reg_name.to_string();
+        while reg_name.len() < 3 {
+            reg_name.push(' ');
+        }
+
+        print!("{} : {} ", reg_name, emu.reg_read(uc_reg).unwrap());
     }
-    Ok(())
+
+    println!("----------------- stack context -----------------");
 }
-*/
 
 fn main() {
-    let engine =
-        Keystone::new(Arch::X86, keystone::keystone_const::MODE_64).expect("Could not initialize Keystone engine");
+    let engine = Keystone::new(Arch::X86, keystone::keystone_const::MODE_64)
+        .expect("Could not initialize Keystone engine");
 
     engine
         .option(OptionType::SYNTAX, keystone::OPT_SYNTAX_NASM)
         .expect("Could not set option to nasm syntax");
 
-
-    /*
-    if let Err(err) = engine.asm("INVALID".to_string(), 0) {
-        println!("Error: {}", err);
-    }
-    */
-
+    let emu = CpuX86::new(unicorn::Mode::MODE_64).expect("failed to instantiate emulator");
     loop {
-        println!("input your commands");
         let mut input = String::new();
         match std::io::stdin().read_line(&mut input) {
             Ok(n) => {
-                //println!("{} bytes read", n);
-                //println!("{}", input);
 
-                let result = engine
-                    .asm(input.to_string(), 0);
+                let result = engine.asm(input.to_string(), 0);
                 match result {
                     Ok(r) => {
-                        println!("ASM result: {:?}", r.bytes);
-                        println!("ASM result: {}", r);
-                    },
-                    Err(e) => {
-                        println!("failed to assemble, err: {:?}", e)
-                    },
+                        let _ = emu.mem_map(0x1000, 0x4000, unicorn::PROT_ALL);
+                        let _ = emu.mem_write(0x1000, &r.bytes);
+
+                        let _ = emu.emu_start(
+                            0x1000,
+                            (0x1000 + r.bytes.len()) as u64,
+                            10 * unicorn::SECOND_SCALE,
+                            1000,
+                        );
+                        print_register(&emu);
+                    }
+                    Err(e) => println!("failed to assemble, err: {:?}", e),
                 }
             }
             Err(error) => println!("error when read your input: {}", error),
         }
         println!();
-        /*
-        if let Err(err) = example() {
-            println!("Error: {}", err);
-        }
-        */
     }
 }
-
-
