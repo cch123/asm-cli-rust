8a63a136f90f852f17c28b798120334cdc70269f
Xargin <cao1988228@163.com>
2019-01-21 15:58:30 +0800

    add license

--- src/machine/interface.rs
+++ src/machine/interface.rs
@@ -144,28 +144,3 @@ impl<'a> Machine<'a> {
     }
 }
 
-/*
-func readFlagVals(flags uint64) map[string]int {
-    res := make(map[string]int)
-    // cf:0 zf:0 of:0 sf:0 pf:0 af:0 df:0
-    flagNames := []string{"cf", "zf", "of", "sf", "pf", "af", "df"}
-    var nameToBitMap = map[string]uint{
-        "cf": 0,
-        "pf": 2,
-        "af": 4,
-        "zf": 6,
-        "sf": 7,
-        "df": 10,
-        "of": 11,
-    }
-    for _, flagName := range flagNames {
-        bitPos := nameToBitMap[flagName]
-
-        res[flagName] = 0
-        if flags>>bitPos&1 > 0 {
-            res[flagName] = 1
-        }
-    }
-    return res
-}
-*/
